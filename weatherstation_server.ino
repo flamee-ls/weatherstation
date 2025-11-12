2ca886f923ce9915eed540901aa1795acfbe0484
flamee-ls <152694449+flamee-ls@users.noreply.github.com>
2025-11-12 16:01:52 +0700

    Update weatherstation_server.ino
    wifi

--- weatherstation_server.ino
+++ weatherstation_server.ino
@@ -1,8 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-const char* ssid = "Maxim";
-const char* password = "26591452";
+const char* ssid = "";
+const char* password = "";
 
 ESP8266WebServer server(80);
 
